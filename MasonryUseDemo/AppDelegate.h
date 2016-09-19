d6488a0a09065fca7a308bfbae0dc6a5bdc2e185
bill <bjh89210@163.com>
2016-09-19 21:16:55 +0800

    创建工程, pod导入 Masonry第三方

---
+++ MasonryUseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryUseDemo
+//
+//  Created by bill on 16/9/19.
+//  Copyright © 2016年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-MasonryUseDemo/Pods-MasonryUseDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MasonryUseDemo : NSObject
+@end
+@implementation PodsDummy_Pods_MasonryUseDemo
+@end
